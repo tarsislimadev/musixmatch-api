ced2861e4b97cd3e0af52771e4fa2f80e25abbff
Tarsis <br.tmvdl@gmail.com>
2023-08-16 20:19:25 -0300

    Adding album.tracks.get request script file

---
+++ src/album.tracks.get.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/sh
+
+# before
+
+method="album.tracks.get"
+
+echo
+echo "method: ${method}"
+echo
+
+echo musixmatch album_id:
+
+read album_id
+
+# while
+
+resp=$( bash musixmatch.sh "${method}" "page_size=80&album_id=${album_id}" | jq )
+
+# after
+
+bash create_file.sh "./data/musixmatch/${method}/${album_id}/" "album_id" "${album_id}"
+
+bash create_file.sh "./data/musixmatch/${method}/${album_id}/" "json" "${resp}"
