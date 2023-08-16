cacde9dfe5f1169f8073e382c1435882d4b3b977
Tarsis <br.tmvdl@gmail.com>
2023-08-16 20:12:37 -0300

    Adding artist.search request script file

---
+++ src/artist.search.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/sh
+
+# before
+
+method="artist.search"
+
+echo
+echo "method: ${method}"
+echo
+
+echo musixmatch artist:
+
+read artist
+
+q_artist=$( echo "${artist}" | sed -e 's/ /+/ig' )
+
+echo
+echo "q_artist: ${q_artist}"
+echo
+
+# while
+
+resp=$( bash musixmatch.sh "${method}" "page_size=80&q_artist=${q_artist}" | jq )
+
+# after
+
+bash create_file.sh "./data/musixmatch/${method}/${q_artist}/" "q_artist" "${q_artist}"
+
+bash create_file.sh "./data/musixmatch/${method}/${q_artist}/" "json" "${resp}"
