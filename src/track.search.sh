d3674259643427a9eef279dc1f909461a195803c
Tarsis <br.tmvdl@gmail.com>
2023-08-16 17:58:07 -0300

    Adding track.search script file

---
+++ src/track.search.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+# before
+
+method="track.search"
+
+q_artist=$( echo "${1}" | sed -e 's/ /+/ig' )
+
+# while
+
+resp=$( bash musixmatch.sh "${method}" "q_artist=${q_artist}" | jq )
+
+# after
+
+bash create_file.sh "./data/api.musixmatch.com/${method}/${q_artist}/" "q_artist" "${q_artist}"
+
+bash create_file.sh "./data/api.musixmatch.com/${method}/${q_artist}/" "json" "${resp}"
