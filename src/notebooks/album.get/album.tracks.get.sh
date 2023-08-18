402aa97876388c938df5c3fd0fd445bb5356ca10
Tarsis <br.tmvdl@gmail.com>
2023-08-18 12:25:25 -0300

    Updating notebooks script files

---
+++ src/notebooks/album.get/album.tracks.get.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+. .env
+
+# inputs
+
+from="album.get"
+
+to="album.tracks.get"
+
+albums=$( ls "${DATABASE}/${from}" )
+
+echo "albums: ${albums}"
+
+# runner
+
+for album in $albums
+
+do
+
+body=$( cat "${DATABASE}/${from}/${album}/json" | jq '.message.body' )
+
+album_id=$( echo "${body}" | jq '.album.album_id' )
+
+echo
+echo "album_id: ${album_id}"
+echo
+
+bash "./src/getters/${to}.sh" "${album_id}"
+
+sleep 2
+
+done
---
+++ src/notebooks/album.tracks.get/track.lyrics.get.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+. .env
+
+# inputs
+
+from="album.tracks.get"
+
+to="track.lyrics.get"
+
+tracks=$( ls "${DATABASE}/${from}" )
+
+echo "tracks: ${tracks}"
+
+# runner
+
+for track in $tracks
+
+do
+
+body=$( cat "${DATABASE}/${from}/${track}/json" | jq '.message.body' )
+
+track_ids=$( echo "${body}" | jq '.track_list[].track.track_id' )
+
+echo
+echo "track_ids: ${track_ids}"
+echo
+
+for track_id in $track_ids
+
+do
+
+echo
+echo "track_id: ${track_id}"
+echo
+
+bash "./src/getters/${to}.sh" "${track_id}"
+
+sleep 2
+
+done
+
+sleep 2
+
+done
