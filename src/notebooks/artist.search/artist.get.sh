1066fd3feed5929cc67f8c50e084051f0b585d93
Tarsis <br.tmvdl@gmail.com>
2023-08-18 10:54:24 -0300

    Updating notebooks script files

---
+++ src/notebooks/artist.search/artist.get.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+. .env
+
+# inputs
+
+from="artist.search"
+
+to="artists"
+
+artists=$( ls "${DATABASE}/${from}" )
+
+echo "artists: ${artists}"
+
+# runner
+
+for artist in $artists
+
+do
+
+body=$( cat "${DATABASE}/${from}/${artist}/json" | jq '.message.body' )
+
+artist_ids=$( echo "${body}" | jq '.artist_list[].artist.artist_id' )
+
+echo
+echo "artist_ids: ${artist_ids}"
+echo
+
+for artist_id in $artist_ids
+
+do
+
+echo
+echo "artist_id: ${artist_id}"
+echo
+
+bash ./src/getters/artist.get.sh "${artist_id}"
+
+sleep 2
+
+done
+
+sleep 2
+
+done
