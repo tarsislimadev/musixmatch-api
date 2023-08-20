01d807eefc3cda7e4df6b827b23fcb4b35a3ab5b
Tarsis <br.tmvdl@gmail.com>
2023-08-20 10:43:46 -0300

    Updating main script file

--- create_file.sh
+++ create_file.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/sh
 
+# echo ". .env"
 . .env
 
+# echo index "${index}"
 index="${1}"
+
+# echo id "${id}"
 id="${2}"
+
+# echo file "${file}"
 file="${3}"
+
+# echo content "${content}"
 content="${4}"
+
+# echo dir "${dir}"
 dir="${DATABASE}/${index}/${id}"
 
 mkdir -p "${dir}"
 touch "${dir}/${file}"
 echo "${content}" > "${dir}/${file}" 
 
-echo written $( expr length "${content}" ) bytes in "${dir}/${file}"
+echo written $( expr length "${content}" ) bytes in "${dir}/${file}" 
