e1c226a8c38aac338d822d122aa174a891d0603f
Tarsis <br.tmvdl@gmail.com>
2023-08-23 20:30:51 -0300

    Update creation of files

--- lines.sh
+++ lines.sh
@@ -2,8 +2,8 @@
 
 echo "${1}" \
  | sed -e 's/\\n/\n\n/ig' \
- | sed -e 's/\. /\n\n/ig' \
- | sed -e 's/\./ /ig' \
- | sed -e 's/\,/ /ig' \
+ | sed -e 's/\. +/. /ig' \
+ | sed -e 's/\, +/, /ig' \
+ | sed -e 's/\! +/! /ig' \
  | sed -e 's/[0-9][0-9]+//ig' \
  | sed -e 's/\*+ This Lyrics is NOT for Commercial use \*+\.?//ig'
